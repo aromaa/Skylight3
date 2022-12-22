93860e73d031fb57637a1524b1617aa7dc00706d
Oliver <jasper.kontturi@gmail.com>
2022-12-22 22:24:35 +0200

    Marketplace Configurations Packet

---
+++ src/Skylight.Server/Game/Communication/Marketplace/GetMarketplaceConfigurationPacketHandler.cs
@@ -0,0 +1,17 @@
+﻿using Net.Communication.Attributes;
+using Skylight.API.Game.Users;
+using Skylight.Protocol.Packets.Incoming.Marketplace;
+using Skylight.Protocol.Packets.Manager;
+using Skylight.Protocol.Packets.Outgoing.Marketplace;
+
+namespace Skylight.Server.Game.Communication.Room.Chat;
+
+[PacketManagerRegister(typeof(AbstractGamePacketManager))]
+internal sealed class GetMarketplaceConfigurationPacketHandler<T> : UserPacketHandler<T>
+	where T : IGetMarketplaceConfigurationIncomingPacket
+{
+	internal override void Handle(IUser user, in T packet)
+	{
+		user.SendAsync(new MarketplaceConfigurationOutgoingPacket(true, 1, 10, 5, 1, 100000, 48, 7, 2, 100000, 40000));
+	}
+}
