baa94b99e54412b47e9fa5c811d15ccfc887832d
Oliver <37178886+OliverSpeed@users.noreply.github.com>
2023-01-10 00:12:41 +0100

    Added more Chat packets for WIN63 (#2)
    * Added Chat networking
    
    * Protocol additions
    
    * Fix bool

---
+++ src/Skylight.Server/Game/Communication/Room/Chat/ShoutPacketHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using Net.Communication.Attributes;
+using Skylight.API.Game.Users;
+using Skylight.Protocol.Packets.Incoming.Room.Chat;
+using Skylight.Protocol.Packets.Manager;
+using Skylight.Protocol.Packets.Outgoing.Room.Chat;
+
+namespace Skylight.Server.Game.Communication.Room.Chat;
+
+[PacketManagerRegister(typeof(AbstractGamePacketManager))]
+internal sealed class ShoutPacketHandler<T> : UserPacketHandler<T>
+	where T : IShoutIncomingPacket
+{
+	internal override void Handle(IUser user, in T packet)
+	{
+		if (user.RoomSession?.Unit is not { } roomUnit)
+		{
+			return;
+		}
+
+		string message = Encoding.UTF8.GetString(packet.Text);
+
+		((Rooms.Room)roomUnit.Room).SendAsync(new ShoutOutgoingPacket(roomUnit.Id, message, 0, packet.StyleId, -1, Array.Empty<(string, string, bool)>()));
+	}
+}
