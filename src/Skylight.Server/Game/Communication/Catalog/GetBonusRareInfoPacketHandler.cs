5e9057cbd8cbfeda079893a40976fc08dcdfc7a7
Oliver <jasper.kontturi@gmail.com>
2024-06-04 08:19:04 +0300

    Profiles

--- src/Skylight.Server/Game/Communication/Catalog/GetBonusRareInfoPacketHandler.cs
+++ src/Skylight.Server/Game/Communication/Catalog/GetBonusRareInfoPacketHandler.cs
@@ -2,6 +2,7 @@
 using Skylight.API.Game.Users;
 using Skylight.Protocol.Packets.Incoming.Catalog;
 using Skylight.Protocol.Packets.Manager;
+using Skylight.Protocol.Packets.Outgoing.Catalog;
 
 namespace Skylight.Server.Game.Communication.Catalog;
 
@@ -11,5 +12,6 @@ internal sealed class GetBonusRareInfoPacketHandler<T> : UserPacketHandler<T>
 {
 	internal override void Handle(IUser user, in T packet)
 	{
+		user.SendAsync(new BonusRareInfoOutgoingPacket("bonusrare16_1_1", 6757, 120, 69));
 	}
 }
---
+++ src/Skylight.Server/Game/Communication/LandingView/GetPromoArticlesPacketHandler.cs
@@ -0,0 +1,21 @@
+﻿using Net.Communication.Attributes;
+using Skylight.API.Game.Users;
+using Skylight.Protocol.Packets.Data.LandingView;
+using Skylight.Protocol.Packets.Incoming.LandingView;
+using Skylight.Protocol.Packets.Manager;
+using Skylight.Protocol.Packets.Outgoing.LandingView;
+
+namespace Skylight.Server.Game.Communication.LandingView;
+
+[PacketManagerRegister(typeof(AbstractGamePacketManager))]
+internal sealed class GetPromoArticlesPacketHandler<T> : UserPacketHandler<T>
+	where T : IGetPromoArticlesIncomingPacket
+{
+	internal override void Handle(IUser user, in T packet)
+	{
+		user.SendAsync(new PromoArticlesOutgoingPacket(new List<PromoArticleData>
+		{
+			new(1, "Skylight", "Brought to you by the Skylight Incorpororated.", "GitHub", 0, "https://github.com/aromaa/Skylight3", "catalogue/attic15_catalog_teaser.gif")
+		}));
+	}
+}
