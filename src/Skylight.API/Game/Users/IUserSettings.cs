ac5b14700f24ee3eb2022dcf0ee181154a4850f2
Oliver <46168061+OliverRetro@users.noreply.github.com>
2025-08-09 15:11:41 +0300

    Sound Settings (#12)

--- src/Skylight.API/Game/Users/IUserSettings.cs
+++ src/Skylight.API/Game/Users/IUserSettings.cs
@@ -3,4 +3,7 @@
 public interface IUserSettings
 {
 	public int HomeRoomId { get; set; }
+	public int UiVolume { get; set; }
+	public int FurniVolume { get; set; }
+	public int TraxVolume { get; set; }
 }
---
+++ src/Skylight.Server/Game/Communication/Preferences/SetSoundSettingsPacketHandler.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Net.Communication.Attributes;
+using Skylight.API.Game.Users;
+using Skylight.Domain.Users;
+using Skylight.Infrastructure;
+using Skylight.Protocol.Packets.Incoming.Preferences;
+using Skylight.Protocol.Packets.Manager;
+
+namespace Skylight.Server.Game.Communication.Preferences;
+
+[PacketManagerRegister(typeof(IGamePacketManager))]
+internal sealed partial class SetSoundSettingsPacketHandler<T>(IDbContextFactory<SkylightContext> dbContextFactory) : UserPacketHandler<T>
+	where T : ISetSoundSettingsIncomingPacket
+{
+	private readonly IDbContextFactory<SkylightContext> dbContextFactory = dbContextFactory;
+
+	internal override void Handle(IUser user, in T packet)
+	{
+		if (packet.UiVolume < 0 || packet.UiVolume > 100 || packet.FurniVolume < 0 || packet.FurniVolume > 100 || packet.TraxVolume < 0 || packet.TraxVolume > 100)
+		{
+			return;
+		}
+
+		int uiVolume = packet.UiVolume;
+		int furniVolume = packet.FurniVolume;
+		int traxVolume = packet.TraxVolume;
+
+		user.Client.ScheduleTask(async client =>
+		{
+			await using SkylightContext dbContext = await this.dbContextFactory.CreateDbContextAsync().ConfigureAwait(false);
+
+			await dbContext.UserSettings.Upsert(new UserSettingsEntity
+			{
+				UserId = client.User!.Profile.Id,
+				UiVolume = uiVolume,
+				FurniVolume = furniVolume,
+				TraxVolume = traxVolume,
+			}).On(c => c.UserId)
+			.WhenMatched((_, c) => new UserSettingsEntity
+			{
+				UiVolume = c.UiVolume,
+				FurniVolume = c.FurniVolume,
+				TraxVolume = c.TraxVolume,
+			}).RunAsync().ConfigureAwait(false);
+
+			client.User.Settings.UiVolume = uiVolume;
+			client.User.Settings.FurniVolume = furniVolume;
+			client.User.Settings.TraxVolume = traxVolume;
+
+			await dbContext.SaveChangesAsync().ConfigureAwait(false);
+		});
+	}
+}
